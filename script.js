e3c9e778a26bfaebee367d9b10d79a414901de17
anmoljpp <anmolnelson@gmail.com>
2025-03-17 10:57:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 const particlesArray = [];
-const particlesPerPart = 29000;
+const particlesPerPart = 39000;
 const numberOfParticles = particlesPerPart * 3;
 
 // Define an array of 10 predefined colors
@@ -73,7 +73,7 @@ function init() {
 
     for (let i = 0; i < particlesPerPart; i++) {
         const col = i % cols;
-        const row = Math.floor(i / cols) * rows;
+        const row = Math.floor(i / cols) % rows;
         const x = col * gridSize + (Math.random() - 0.5) * gridSize;
         const y = row * gridSize + (Math.random() - 0.5) * gridSize;
         particlesArray.push(new Particle(x, y));
@@ -81,7 +81,7 @@ function init() {
 
     for (let i = 0; i < particlesPerPart; i++) {
         const col = i % cols;
-        const row = Math.floor(i / cols);
+        const row = Math.floor(i / cols) % rows;
         const x = col * gridSize + (Math.random() - 0.5) * gridSize;
         const y = thirdHeight + row * gridSize + (Math.random() - 0.5) * gridSize;
         particlesArray.push(new Particle(x, y));
@@ -89,7 +89,7 @@ function init() {
 
     for (let i = 0; i < particlesPerPart; i++) {
         const col = i % cols;
-        const row = Math.floor(i / cols);
+        const row = Math.floor(i / cols) % rows;
         const x = col * gridSize + (Math.random() - 0.5) * gridSize;
         const y = 2 * thirdHeight + row * gridSize + (Math.random() - 0.5) * gridSize;
         particlesArray.push(new Particle(x, y));
