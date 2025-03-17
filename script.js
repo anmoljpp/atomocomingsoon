6f413e9ae2f356ad027362b7a9bc133a5e6ae7c2
anmoljpp <anmolnelson@gmail.com>
2025-03-17 10:56:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -73,7 +73,7 @@ function init() {
 
     for (let i = 0; i < particlesPerPart; i++) {
         const col = i % cols;
-        const row = Math.floor(i / cols) % rows;
+        const row = Math.floor(i / cols) * rows;
         const x = col * gridSize + (Math.random() - 0.5) * gridSize;
         const y = row * gridSize + (Math.random() - 0.5) * gridSize;
         particlesArray.push(new Particle(x, y));
